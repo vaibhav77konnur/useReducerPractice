032d18bb5ecec1d6240ba061b434f72859f5bef2
Vaibhav Konnur <83021535+vaibhav77konnur@users.noreply.github.com>
2024-12-23 21:42:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import DateCounter from "./DateCounter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DateCounter />
+    </div>
+  );
+}
